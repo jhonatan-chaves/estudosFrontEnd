378e9a1a376961372e8acb6802a04ef4dca86a08
jhonatan-chaves <cjonata799@gmail.com>
2023-02-14 14:04:12 -0300

    adicionando javaScript ao conteudo de estudos

---
+++ estudosJavaSc/index.js
@@ -0,0 +1,34 @@
+
+
+function soma(num1,num2) {
+    return num1 + num2  
+    
+}
+
+
+function subtracao(num1,num2) {
+  return num1 - num2  
+  
+}
+
+function multiplicacao(num1,num2) {
+  return num1 * num2  
+  
+}
+
+function divisao(num1,num2) {
+  return num1 / num2  
+  
+}
+
+console.log(`soma:${soma(25,75)}`);
+console.log("====================================")
+console.log(`subtração: ${subtracao(25,5)}`);
+console.log("====================================")
+console.log(`multiplicação: ${multiplicacao(10,20)}`);
+console.log("====================================")
+console.log(`divisão: ${divisao(20,5)}`);
+
+
+
+
