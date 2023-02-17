497f9a071b40b16c19e41acf6b480408a31b46bf
jhonatan-chaves <cjonata799@gmail.com>
2023-02-17 15:55:05 -0300

    concertando alguns argumentos

--- estudosJsaula4/index.js
+++ estudosJsaula4/index.js
@@ -23,35 +23,32 @@ let idade = 28;
 let possuiDoutorado = false;
 let empregoFuturo;
 let dinheiroNaConta = 0;
+console.log(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta);
 
-if(nome){
-  console.log("verdadeiro");
+//compare o tota de habitantes do Brasil com a China (valor em milhas)
+
+
+
+
+let brasil = 20755e6;
+let china = 1340e6;
+
+if(brasil > china){
+  console.log("O Brasil tem mais habitantes que a China " + brasil.toLocaleString("pt-BR", {style:"decimal"}) + "de habitantes");
 }else{
-  console.log("falso")
+  console.log("A China tem o maior numero de habitantes se compardo com o Brasil  totalizando: " + china.toLocaleString("pt-BR",{style:"decimal"}) + " de habitantes");
 }
-
-if(idade){
-  console.log("verdadeiro");
+// verifique oque irá aparecer no console?
+if("Gato" === "gato" && (5 >2)){
+  console.log("vardadeiro");
+}else{
+  console.log("falso");
+}
+//verifique oque ira aparecer no comsole?
+if("Gato" === "gato" || (5 > 2)){
+  console.log("Gato" && "Cão");
 }else{
   console.log("falso");
 }
 
-if(possuiDoutorado){
-  console.log("verdadeiro");
-}else{
-  console.log("falso");
-}
-
-if(empregoFuturo){
-  console.log("verdadeiro");
-}else{
-  console.log("falso");
-}
-
-if(dinheiroNaConta){
-  console.log("verdadeiro");
-}else{
-  console.log("falso");
-}
-
-//
+ 
